0eccc71ed23de26b77dccb0c9c74201320cb113d
Alexander Sysoyev <sysoyev@vmk.unn.ru>
2015-10-06 20:20:21 +0300

    fix tests names, fix code tab size

--- test/test_evolvent.cpp
+++ test/test_evolvent.cpp
@@ -3,78 +3,78 @@
 
 class TEvolventTest : public ::testing::Test 
 {
- protected:
-    TEvolvent* evolvent;
-    TEvolventTest() : evolvent(0) {}
-    void CreateEvolvent(int N, int m) 
-    {
-        const double A[] = {-0.5, -0.5}, B[] = {0.5, 0.5};
-        evolvent = new TEvolvent(N, m);  
-        evolvent->SetBounds(A, B);
-    }
-    ~TEvolventTest()
-    {
-        delete evolvent;
-    }
+protected:
+  TEvolvent* evolvent;
+  TEvolventTest() : evolvent(0) {}
+  void CreateEvolvent(int N, int m) 
+  {
+    const double A[] = {-0.5, -0.5}, B[] = {0.5, 0.5};
+    evolvent = new TEvolvent(N, m);  
+    evolvent->SetBounds(A, B);
+  }
+  ~TEvolventTest()
+  {
+    delete evolvent;
+  }
 };
 
 
 /*Проверка входных параметров в конструкторе TEvolvent(int _N, int _m)*/
-TEST(TEvolvent, throws_when_create_with_negative_N)
+TEST_F(TEvolventTest, throws_when_create_with_negative_N)
 {
-    ASSERT_ANY_THROW(TEvolvent ev(-1, 10));
+  ASSERT_ANY_THROW(TEvolvent ev(-1, 10));
 }
 
-TEST(TEvolvent, throws_when_create_with_too_large_N)
+TEST_F(TEvolventTest, throws_when_create_with_too_large_N)
 {
-    ASSERT_ANY_THROW(TEvolvent ev(MaxDim + 1, 10));
+  ASSERT_ANY_THROW(TEvolvent ev(MaxDim + 1, 10));
 }
 
-TEST(TEvolvent, throws_when_create_with_too_low_m)
+TEST_F(TEvolventTest, throws_when_create_with_too_low_m)
 {
-    ASSERT_ANY_THROW(TEvolvent ev(2, 1));
+  ASSERT_ANY_THROW(TEvolvent ev(2, 1));
 }
 
-TEST(TEvolvent, throws_when_create_with_too_large_m)
+TEST_F(TEvolventTest, throws_when_create_with_too_large_m)
 {
-    ASSERT_ANY_THROW(TEvolvent ev(2, MaxM + 1));
+  ASSERT_ANY_THROW(TEvolvent ev(2, MaxM + 1));
 }
 
-TEST(TEvolvent, can_create_with_correct_values)
+TEST_F(TEvolventTest, can_create_with_correct_values)
 {
-    ASSERT_NO_THROW(TEvolvent ev(MaxDim - 1, MaxM - 1));
+  ASSERT_NO_THROW(TEvolvent ev(MaxDim - 1, MaxM - 1));
 }
 
 /*Проверка GetImage(const double& x, double* _y)*/
 TEST_F(TEvolventTest, can_get_Y_on_correct_X)
 {    
-    const int N = 2, m = 2;
-    const double true_value[N] = {-0.125, -0.375};
-    double x = 0.1;
-    double* y = new double[N];
-    CreateEvolvent(N, m);
-    evolvent->GetImage(x, y);
-    for(int i = 0; i < N; i++)
-        EXPECT_EQ(true_value[i], y[i]);
-    delete []y;
+  const int N = 2, m = 2;
+  const double true_value[N] = {-0.125, -0.375};
+  double x = 0.1;
+  double* y = new double[N];
+  CreateEvolvent(N, m);
+  evolvent->GetImage(x, y);
+  for (int i = 0; i < N; i++)
+    EXPECT_EQ(true_value[i], y[i]);
+  delete [] y;
 }
 
 TEST_F(TEvolventTest, throws_when_get_image_with_negative_x)
 {    
-    const int N = 2, m = 2;
-    double x = -1;
-    double* y = new double[N];
-    CreateEvolvent(N, m);
-    ASSERT_ANY_THROW(evolvent->GetImage(x, y));
-    delete []y;
+  const int N = 2, m = 2;
+  double x = -1;
+  double* y = new double[N];
+  CreateEvolvent(N, m);
+  ASSERT_ANY_THROW(evolvent->GetImage(x, y));
+  delete [] y;
 }
 
 TEST_F(TEvolventTest, throws_when_get_image_with_too_large_x)
 {    
-    const int N = 2, m = 2;
-    double x = 2;
-    double* y = new double[N];
-    CreateEvolvent(N, m);
-    ASSERT_ANY_THROW(evolvent->GetImage(x, y));
-    delete []y;
+  const int N = 2, m = 2;
+  double x = 2;
+  double* y = new double[N];
+  CreateEvolvent(N, m);
+  ASSERT_ANY_THROW(evolvent->GetImage(x, y));
+  delete []y;
 }
