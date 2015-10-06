08a6d5635b9ee365754ab4f03d9d94f4829691f1
Alexander Sysoyev <sysoyev@vmk.unn.ru>
2015-10-06 20:21:09 +0300

    change file header, fix defenitions

--- include/common.h
+++ include/common.h
@@ -1,56 +1,48 @@
-﻿/////////////////////////////////////////////////////////////////////////////
-//                                                                         //
-//             LOBACHEVSKY STATE UNIVERSITY OF NIZHNY NOVGOROD             //
-//                                                                         //
-//                       Copyright (c) 2015 by UNN.                        //
-//                          All Rights Reserved.                           //
-//                                                                         //
-//  File:      common.h                                                    //
-//                                                                         //
-//  Purpose:   Common Header file                                          //
-//                                                                         //
-//  Author(s): Sysoyev A., Barkalov K.                                     //
-//                                                                         //
-/////////////////////////////////////////////////////////////////////////////
+﻿/*
+* Copyright (C) 2015 University of Nizhni Novgorod
+* This file is part of the Globalizer Light library, v 1.0
+* 
+* common.h - common defenitions
+* 
+* This file may be used under the terms of the GNU Lesser General Public 
+* License version 2.1 as published by the Free Software Foundation 
+* and appearing in the file LICENSE.LGPL included in the packaging of this 
+* file. Please review the following information to ensure the GNU Lesser 
+* General Public License version 2.1 requirements will be met: 
+* http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+*/ 
 
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-/* ============================================================================================= *\
-**  Constants                                                                                    **
-\* ============================================================================================= */
+/* ========================================================================= *\
+**  Constants                                                                **
+\* ========================================================================= */
 
-//const int MaxPathLength = 512
-const int MaxNumOfTaskLevels = 5;
-const int MaxNumOfFunc = 20;
-const int MaxDim = 50;
+//const int MaxNumOfTaskLevels = 5;
+const int MaxNumOfFunc = 1; // в настоящий момент решаются только безусловные
+                            //   задачи, поэтому функционал один
+const int MaxDim = 5;
 const int MaxM = 15;
 const int MaxL = 10;
 
 const double MaxDouble = 1.7e308;
 
-const int DefaultQueueSize = 16777215;//8388607;//1048573;//8388607;//524287; //262143; // должно быть равно 2^k - 1
+const int DefaultQueueSize = 1048575; // should be 2^k - 1
 const int DefaultSearchDataSize = 100000;
 
-const int TagChildStartSolve = 101;
-const int TagChildSolved  = 102;
-const int ChildStopMsg = -101;
-
-/* ============================================================================================= *\
-**  Types                                                                                        **
-\* ============================================================================================= */
-//typedef double (*TFuncPointer)(const double*, int);
+/* ========================================================================= *\
+**  Types                                                                    **
+\* ========================================================================= */
 
 class TProcess;
 #ifdef WIN32
 typedef void (__cdecl *tIterationHandler)(TProcess *pProcess);
 typedef double (__cdecl *tFunction)(const double*);
 #else
-typedef void ( *tIterationHandler)(TProcess *pProcess);
-typedef double ( *tFunction)(const double*);
+typedef void (*tIterationHandler)(TProcess *pProcess);
+typedef double (*tFunction)(const double*);
 #endif
-
-
 
 #endif
 // - end of file ----------------------------------------------------------------------------------
